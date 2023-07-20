020a5868e93d01193684c642d16b6de1f4dbdc7c
MauricioPassosF <mauricio.passos97@gmail.com>
2023-07-20 16:19:52 -0300

    Req 1 0k

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 const express = require('express');
-const arrayJson = require('./talker.json');
+const fs = require('fs').promises;
 
-console.log(arrayJson);
+const talkerJsonPath = './src/talker.json';
+
+const readFileFunc = async (path) => {
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+    // console.log(data);
+    return data;
+  } catch (err) {
+    console.error(`Erro ao ler o arquivo: ${err.message}`);
+  }
+};
+
+// readFileFunc(talkerJsonPath);
 
 const app = express();
+
 app.use(express.json());
+
+app.get('/talker', async (req, res) => {
+  const data = await readFileFunc(talkerJsonPath);
+  res.status(200).send(JSON.parse(data));
+});
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
