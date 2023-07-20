a1b5b09f09ffeca7a3b0713e093d32aa67c45900
MauricioPassosF <mauricio.passos97@gmail.com>
2023-07-20 17:34:58 -0300

    Req 3 semiok

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const fs = require('fs').promises;
 
 const talkerJsonPath = './src/talker.json';
@@ -11,6 +12,10 @@ const readFileFunc = async (path) => {
     console.error(`Erro ao ler o arquivo: ${err.message}`);
   }
 };
+
+// const getRandomCrypto = () => cryptoRandomString({ length: 16 });
+const getRandomCrypto = () => Math.random().toString(16).substr(2) 
++ Math.random().toString(16).substr(2, 3);
 
 const app = express();
 
@@ -25,12 +30,15 @@ app.get('/talker/:id', async (req, res) => {
   const { id } = req.params;
   const talkers = await readFileFunc(talkerJsonPath);
   const person = talkers.filter((talker) => talker.id === Number(id)); 
-  console.log(person);
 
   if (person.length === 0) {
     res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
   res.status(200).send(person[0]);
+});
+
+app.post('/login', (req, res) => {
+  res.status(200).json({ token: getRandomCrypto() });
 });
 
 const HTTP_OK_STATUS = 200;
